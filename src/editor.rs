f7322031214b9736dffcd7127add59397d9a8e62
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2022-08-24 20:29:42 +0200

    Derive default for Position

--- src/editor.rs
+++ src/editor.rs
@@ -3,6 +3,7 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -19,7 +20,7 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal."),
-            cursor_position: Position { x: 0, y: 0 },
+            cursor_position: Position::default(),
         }
     }
 
@@ -85,7 +86,7 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
 
         if self.should_quit {
             Terminal::clear_screen();

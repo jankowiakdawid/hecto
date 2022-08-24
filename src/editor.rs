4d71ebe14bcb131d5c80d2da6aa63d5bc18429f1
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2022-08-24 08:58:22 +0200

    Prevent cursor from going off screen

--- src/editor.rs
+++ src/editor.rs
@@ -3,7 +3,6 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -45,25 +44,37 @@ impl Editor {
             Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(pressed_key),
             _ => (),
         }
-        
+
         Ok(())
     }
 
     fn move_cursor(&mut self, key: Key) {
-        let Position {mut x, mut y} = self.cursor_position;
+        let Position { mut x, mut y } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+
         match key {
             Key::Up => y = y.saturating_sub(1),
-            Key::Down => y = y.saturating_add(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1)
+                }
+            }
             Key::Left => x = x.saturating_sub(1),
-            Key::Right => x = x.saturating_add(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1)
+                }
+            }
             _ => (),
         }
-        self.cursor_position = Position {x, y}
+        self.cursor_position = Position { x, y }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position {x: 0, y: 0});
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
 
         if self.should_quit {
             Terminal::clear_screen();
